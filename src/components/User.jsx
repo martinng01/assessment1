cd197b5685127ab9aea469d408d2f85fd66b0650
Martin <martin.ng@live.com>
2024-09-18 16:15:59 +0800

    Add User unit test

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,9 +5,9 @@ const User = ({ name, username, email }) => {
   return (
     <div className="card bg-base-100 w-96 shadow-xl bg-white">
       <div className="card-body">
-        <p>Name: {name}</p>
-        <p>Username: {username}</p>
-        <p>Email: {email}</p>
+        <p>Name: {name.trim()}</p>
+        <p>Username: {username.trim()}</p>
+        <p>Email: {email.trim()}</p>
       </div>
     </div>
   );
---
+++ src/tests/User.test.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import User from "../components/User";
+import "@testing-library/jest-dom";
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+describe("User component", () => {
+  it("renders user details", () => {
+    const name = "John Doe";
+    const username = "johndoe";
+    const email = "johndoe@gmail.com";
+
+    render(<User name={name} username={username} email={email} />);
+    const nameElement = screen.getByText(`Name: ${name}`);
+    const usernameElement = screen.getByText(`Username: ${username}`);
+    const emailElement = screen.getByText(`Email: ${email}`);
+
+    expect(nameElement).toBeInTheDocument();
+    expect(usernameElement).toBeInTheDocument();
+    expect(emailElement).toBeInTheDocument();
+  });
+
+  it("renders user details without leading and trailing whitespaces", () => {
+    const name = "  John Doe  ";
+    const username = "  johndoe  ";
+    const email = "  johndoe@gmail.com  ";
+
+    render(<User name={name} username={username} email={email} />);
+    const nameElement = screen.getByText(`Name: ${name.trim()}`);
+    const usernameElement = screen.getByText(`Username: ${username.trim()}`);
+    const emailElement = screen.getByText(`Email: ${email.trim()}`);
+
+    expect(nameElement).toBeInTheDocument();
+    expect(usernameElement).toBeInTheDocument();
+    expect(emailElement).toBeInTheDocument();
+  });
+});
